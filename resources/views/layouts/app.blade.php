a92685d0f0ffd234e29eb919bac55f45679194f9
baonq0507 <nquangbao0507@gmail.com>
2024-12-27 21:34:25 +0700

    d

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,7 @@
             <!-- Chat --> --}}
 
         <!-- Chat -->
-        <main class="main is_visible" data-dropzone-area="">
+        <main class="main " data-dropzone-area="">
             <div class="container h-100">
                 @yield('content')
             </div>
